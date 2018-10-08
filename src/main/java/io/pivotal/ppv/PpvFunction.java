c8fead050f71ac04b4aa7d7aeae790fc5d717173
Mayuresh Krishna <mayureshkrishna@gmail.com>
2018-10-08 19:42:07 -0400

    Added try/catch

--- src/main/java/io/pivotal/ppv/PpvFunction.java
+++ src/main/java/io/pivotal/ppv/PpvFunction.java
@@ -1,6 +1,6 @@
 package io.pivotal.ppv;
 
-import java.util.Optional;
+import java.util.List;
 import java.util.function.Function;
 
 import org.apache.commons.logging.Log;
@@ -13,53 +13,42 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class PpvFunction implements Function<String, String> {
 
 	private Log logger = LogFactory.getLog(PpvFunction.class);
-	
+
 	@Autowired
 	private PpvRepository ppvRepository;
-	
+
 	public String apply(String id) {
 		String ppvAsJsonString = null;
-		
-		logger.info("Inside Apply - Input Id: "+id);
-		
-		
+
+		logger.info("Inside Apply - Input Id: " + id);
+
 		try {
-		
-		
-		long longId = Long.parseLong(id);
-		
-		Optional<Ppv> ppv = ppvRepository.findById(longId);
-		
-		logger.info("Find By Id Invoked");
-		
-		if (ppv.isPresent())
-		{	
-			logger.info("PPV Present for ID:"+ longId);
-			ObjectMapper objectMapper = new ObjectMapper();
-			
-			
-			
-				ppvAsJsonString = objectMapper.writeValueAsString(ppv.get());
-			
-				logger.info("Retrieved: "+ ppvAsJsonString);
-		}
-		else 
-			
-		{
-			ppvAsJsonString = "{\"msg\":\"No Data Found\"\"}";
-			logger.info("No Data Found");
-		}
+
+			// long longId = Long.parseLong(id);
+
+			List<Ppv> ppv = ppvRepository.findAll();
+
+			logger.info("Find All Invoked");
+
+			if (!ppv.isEmpty()) {
+				ObjectMapper objectMapper = new ObjectMapper();
+
+				ppvAsJsonString = objectMapper.writeValueAsString(ppv);
+
+				logger.info("Retrieved: " + ppvAsJsonString);
+			} else {
+				ppvAsJsonString = "{\"msg\":\"No Data Found\"\"}";
+				logger.info("No Data Found");
+			}
 		} catch (JsonProcessingException jpe) {
 			ppvAsJsonString = jpe.getMessage();
-			logger.error("PPV Info Failed: "+ppvAsJsonString);
-		}
-		catch (Exception e) {
+			logger.error("PPV Info Failed: " + ppvAsJsonString);
+		} catch (Exception e) {
 			ppvAsJsonString = e.getMessage();
-			logger.error("PPV Info Failed: "+ppvAsJsonString);
+			logger.error("PPV Info Failed: " + ppvAsJsonString);
 
 		}
 		return ppvAsJsonString;
 	}
-	
-	
+
 }
