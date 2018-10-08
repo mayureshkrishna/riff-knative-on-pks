70cb48e6eb673142fa84f84bf7dcffddeb153331
Mayuresh Krishna <mayureshkrishna@gmail.com>
2018-10-08 13:07:25 -0400

    simplify for it to work

--- src/main/java/io/pivotal/ppv/PpvFunction.java
+++ src/main/java/io/pivotal/ppv/PpvFunction.java
@@ -3,9 +3,9 @@ package io.pivotal.ppv;
 import java.util.function.Function;
 
 
-public class PpvFunction implements Function<Long, Ppv> {
+public class PpvFunction implements Function<String, Ppv> {
 
-	public Ppv apply(Long id) {
+	public Ppv apply(String id) {
 		Ppv ppv = new Ppv();
 		ppv.setId(1L);
 		ppv.setName("UFC:Khabib vs. McGregor");
