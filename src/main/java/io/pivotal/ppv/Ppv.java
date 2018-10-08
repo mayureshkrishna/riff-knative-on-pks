f8e0acd75116e47ce196f533ad8d2b8879e80499
Mayuresh Krishna <mayureshkrishna@gmail.com>
2018-10-08 13:00:16 -0400

    simplify for it to work

--- src/main/java/io/pivotal/ppv/Ppv.java
+++ src/main/java/io/pivotal/ppv/Ppv.java
@@ -1,22 +1,35 @@
 package io.pivotal.ppv;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-//@Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Ppv {
 
-
-		//@Id
-		//@GeneratedValue
 		private Long id;
 		private String name;
 		private String price;
 		private String dateOfShowing;
+		public Long getId() {
+			return id;
+		}
+		public void setId(Long id) {
+			this.id = id;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getPrice() {
+			return price;
+		}
+		public void setPrice(String price) {
+			this.price = price;
+		}
+		public String getDateOfShowing() {
+			return dateOfShowing;
+		}
+		public void setDateOfShowing(String dateOfShowing) {
+			this.dateOfShowing = dateOfShowing;
+		}
 
 	
 }
