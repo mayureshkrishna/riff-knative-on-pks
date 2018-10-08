bb96b6a6c0b20444c22d09848c14c3b28991503f
Mayuresh Krishna <mayureshkrishna@gmail.com>
2018-10-08 12:54:44 -0400

    simplify for it to work

--- src/main/java/io/pivotal/ppv/PpvFunction.java
+++ src/main/java/io/pivotal/ppv/PpvFunction.java
@@ -5,25 +5,7 @@ import java.util.function.Function;
 
 public class PpvFunction implements Function<Long, Ppv> {
 
-	//private Log logger = LogFactory.getLog(PpvFunction.class);
-
-/*	@Autowired
-	private final PpvRepository ppvRepository;
-
-	PpvFunction(PpvRepository ppvRepository) {
-		this.ppvRepository = ppvRepository;
-	}*/
-
 	public Ppv apply(Long id) {
-		/*Optional<Ppv> ppv = ppvRepository.findById(id);
-		if (ppv.isPresent()) {
-			logger.info(" Retrieved PPV Info for following: " + ppv.get().getName());
-			return ppv.get();
-		} else {
-			logger.info(" No PPV Info found for Id: " + id);
-			return null;
-		}*/
-		
 		Ppv ppv = new Ppv();
 		ppv.setId(1L);
 		ppv.setName("UFC:Khabib vs. McGregor");
--- src/main/java/io/pivotal/ppv/PpvRepository.java
+++
@@ -1,8 +0,0 @@
-package io.pivotal.ppv;
-
-/*public interface PpvRepository extends JpaRepository<Ppv, Long>{
-	Collection<Ppv> findByName(String name);
-*/
-public class PpvRepository {
-	
-}
