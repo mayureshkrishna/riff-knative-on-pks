06e01e2d992b068bc31ce6000e44fa2083fb9aec
Mayuresh Krishna <mayureshkrishna@gmail.com>
2018-10-09 10:10:23 -0400

    Added get and save functions

--- src/main/java/io/pivotal/ppv/PpvApplication.java
+++ src/main/java/io/pivotal/ppv/PpvApplication.java
@@ -1,7 +1,10 @@
 package io.pivotal.ppv;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -19,11 +22,10 @@ public class PpvApplication {
 	private PpvRepository ppvRepository;
 
 	@Bean
-	public Function<String, List<PayPerView>> payperview() {
+	public Function<String, List<PayPerView>> getpayperviewbyname() {
 
 		return name -> {
 
-			String ppvAsJsonString = null;
 			try {
 				List<PayPerView> ppv = ppvRepository.findByName(name);
 				logger.info("Number of records: " + ppv.size());
@@ -41,11 +43,82 @@ public class PpvApplication {
 			} 
 			
 			catch (Exception e) {
-				ppvAsJsonString = e.getMessage();
-				logger.error("PPV Info Failed: " + ppvAsJsonString);
+				logger.error("PPV Info Failed: " + e.getMessage());
 				return null;
 
 			}
+		};
+	}
+	
+	@Bean
+	public Function <Long,PayPerView> getpayperviewbyid() {
+
+		return id -> {
+			try {
+				Optional<PayPerView> ppv = ppvRepository.findById(id);
+				if (ppv.isPresent()) {
+					
+					logger.info("Retrieved: " + ppv.get().toString());
+					return ppv.get();
+				} else {
+					logger.info("No Data Found");
+					return null;
+				}
+			} 
+			catch (Exception e) {
+				logger.error("PPV Info Failed: " + e.getMessage());
+				return null;
+			}
+		};
+	}
+	
+	@Bean
+	public Supplier <List<PayPerView>> getpayperview() {
+		
+		return() -> {
+		try {
+			List<PayPerView> ppv = ppvRepository.findAll();
+			logger.info("Number of records: " + ppv.size());
+
+			if (ppv.isEmpty()) {
+				logger.info("No Data Found");
+				return null;
+
+			} else {
+
+				logger.info("Retrieved: " + ppv.toString());
+				return ppv;
+				
+			}
+		} 
+		
+		catch (Exception e) {
+			logger.error("PPV Info Failed: " + e.getMessage());
+			return null;
+
+		}
+		};
+	}
+	
+	@Bean
+	public Consumer <PayPerView> savepayperview() {
+		
+		return ppv ->
+		{
+		try {
+			if (ppv == null) {
+				logger.info("No Data Provided");
+
+			} else {
+				
+				ppvRepository.save(ppv);
+				logger.info("Saved: " + ppv.toString());
+			}
+		} 
+		
+		catch (Exception e) {
+			logger.error("PPV Info Failed: " + e.getMessage());
+		}
 		};
 	}
 
