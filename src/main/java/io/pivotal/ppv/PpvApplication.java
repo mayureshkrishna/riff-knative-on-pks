816904ef61b007e26d8348e59b0d61c6c9328350
Mayuresh Krishna <mayureshkrishna@gmail.com>
2018-10-08 21:39:58 -0400

    Trying config

--- src/main/java/io/pivotal/ppv/PpvApplication.java
+++ src/main/java/io/pivotal/ppv/PpvApplication.java
@@ -1,16 +1,27 @@
 package io.pivotal.ppv;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class PpvApplication {
+	
+	@Autowired
+	private ConfigurableApplicationContext context;
 	
 	public static void main(String[] args) {
 		
 		
 		SpringApplication.run(PpvApplication.class, args);
 		
+	}
+	@PostConstruct
+	public void init() {
+		context.registerShutdownHook();
 	}
 }
 
